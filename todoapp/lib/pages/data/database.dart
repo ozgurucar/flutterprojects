b7a0559ceb0678fcf106a188db0c4ec40eec4b22
Ozgur Ucar <ucar.ozgur@outlook.com>
2024-04-07 18:06:11 +0300

    todoapp with local database

---
+++ todoapp/lib/pages/data/database.dart
@@ -0,0 +1,23 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+  List toDoList = [];
+
+// reference box
+  final _myBox = Hive.box('myBox');
+
+  void createInitialData() {
+    toDoList = [
+      ["Make Tutorial", false],
+      ["Do Exercise", false]
+    ];
+  }
+
+  void loadData() {
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  void updateDataBase() {
+    _myBox.put("TODOLIST", toDoList);
+  }
+}
